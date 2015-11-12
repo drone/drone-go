3e1a2cf2ef14bf2267da9f046156bb4335f2dd45
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-11-12 11:42:18 -0800

    combined string or map json type

--- drone/types_json.go
+++ drone/types_json.go
@@ -28,6 +28,13 @@ func (e *StringSlice) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+func (e *StringSlice) Len() int {
+	if e == nil {
+		return 0
+	}
+	return len(e.parts)
+}
+
 func (e *StringSlice) Slice() []string {
 	if e == nil {
 		return nil
@@ -52,4 +59,52 @@ func (e *StringInt) UnmarshalJSON(b []byte) error {
 
 func (e StringInt) String() string {
 	return e.value
+}
+
+// StringMap representes a string or a map of strings.
+// StringMap representes a string or a map of strings.
+type StringMap struct {
+	parts map[string]string
+}
+
+func (e *StringMap) UnmarshalJSON(b []byte) error {
+	if len(b) == 0 {
+		return nil
+	}
+
+	p := map[string]string{}
+	if err := json.Unmarshal(b, &p); err != nil {
+		var s string
+		if err := json.Unmarshal(b, &s); err != nil {
+			return err
+		}
+		p[""] = s
+	}
+
+	e.parts = p
+	return nil
+}
+
+func (e *StringMap) Len() int {
+	if e == nil {
+		return 0
+	}
+	return len(e.parts)
+}
+
+func (e *StringMap) String() (str string) {
+	if e == nil {
+		return
+	}
+	for _, val := range e.parts {
+		return val // returns the first string value
+	}
+	return
+}
+
+func (e *StringMap) Map() map[string]string {
+	if e == nil {
+		return nil
+	}
+	return e.parts
 }
